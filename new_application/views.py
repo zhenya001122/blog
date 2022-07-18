2c7dc20365e2469e6866af4e0cdd30e8a12bb4ce
Evgeniy Shutko <sluckltu@gmail.com>
2022-07-18 23:24:00 +0300

    add logger

--- new_application/views.py
+++ new_application/views.py
@@ -1,7 +1,13 @@
+import logging
 from django.shortcuts import render
 from django.http import HttpResponse
 
+logger = logging.getLogger(__name__)
+
 def home(request):
+    logger.error("Test!!")
     return HttpResponse('Главная')
+
 def about(request):
+    logger.error("Test_2!!")
     return HttpResponse('Наш клуб')

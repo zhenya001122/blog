18a3f666bfac562fda5cf647bc4bab08e80bd749
Evgeniy Shutko <sluckltu@gmail.com>
2022-07-20 16:16:10 +0300

    blog

--- new_application/views.py
+++ new_application/views.py
@@ -13,8 +13,11 @@ def about(request):
     return HttpResponse('Наш клуб')
 
 def users(request):
-    logger.error("Запрошена страница 'Пользователи'")
-    id = request.GET.get("id", 1)
-    name = request.GET.get("name", "Tom")
-    output = "User id: {0}  name: {1}".format(id, name)
-    return HttpResponse(output)
+    if request.method == "GET":
+        logger.error("Запрошена страница 'Пользователи'")
+        id = request.GET.get("id", 1)
+        name = request.GET.get("name", "Zhenya")
+        output = "User id: {0}  name: {1}".format(id, name)
+    # return HttpResponse(output)
+    # elif request.method == 'POST':
+

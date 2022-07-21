30caf9ab3c010c48b2c71b1feaf700f615d59eea
Evgeniy Shutko <sluckltu@gmail.com>
2022-07-22 00:00:45 +0300

    blog

--- posts/views.py
+++ posts/views.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 def index(request):
     # if logger.info({settings.BBBB}):
-    logger.info(f"MY_ENV_VAR: {settings.MY_ENV_VAR}") #возвращает в консоль значение None
+    logger.info(f"MY_ENV_VAR: {settings.MY_ENV_VAR}")
     if request.GET.get("key") == "test":
         return HttpResponse("Posts with test key")
     return HttpResponse("Posts index view")
